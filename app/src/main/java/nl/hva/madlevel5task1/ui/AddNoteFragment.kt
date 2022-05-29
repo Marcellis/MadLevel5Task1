a1f28fc6d7ac8d382355bcc716c9cebe873ad4ba
marcomarcellis <m.m.c.m.marcellis@hva.nl>
2022-05-29 13:55:08 +0200

    Initial commit

---
+++ app/src/main/java/nl/hva/madlevel5task1/ui/AddNoteFragment.kt
@@ -0,0 +1,69 @@
+package nl.hva.madlevel5task1.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.madlevel5task1.databinding.FragmentAddNoteBinding
+import nl.hva.madlevel5task1.vm.NoteViewModel
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class AddNoteFragment : Fragment() {
+
+    private var _binding: FragmentAddNoteBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: NoteViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAddNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnSave.setOnClickListener {
+            saveNote()
+        }
+
+        observeNote()
+    }
+
+    private fun observeNote() {
+        //fill the text fields with the current text and title from the viewmodel
+        viewModel.note.observe(viewLifecycleOwner, { note ->
+            note?.let {
+                binding.tilNoteTitle.editText?.setText(it.title)
+                binding.tilNoteText.editText?.setText(it.text)
+            }
+
+        })
+
+        viewModel.error.observe(viewLifecycleOwner, { message ->
+            Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
+        })
+
+        viewModel.success.observe(viewLifecycleOwner, {
+            //"pop" the backstack, this means we destroy this    fragment and go back to the RemindersFragment
+            findNavController().popBackStack()
+        })
+    }
+
+    private fun saveNote() {
+        viewModel.updateNote(
+            binding.tilNoteTitle.editText?.text.toString(),
+            binding.tilNoteText.editText?.text.toString()
+        )
+    }
+
+}
---
+++ app/src/main/java/nl/hva/madlevel5task1/ui/NotepadFragment.kt
@@ -0,0 +1,52 @@
+package nl.hva.madlevel5task1.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.madlevel5task1.R
+import com.example.madlevel5task1.databinding.FragmentNotepadBinding
+import nl.hva.madlevel5task1.vm.NoteViewModel
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class NotepadFragment : Fragment() {
+
+    private var _binding: FragmentNotepadBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: NoteViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentNotepadBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeAddNoteResult()
+
+//        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
+    }
+
+    private fun observeAddNoteResult() {
+        viewModel.note.observe(viewLifecycleOwner, { note ->
+            note?.let {
+                binding.tvNoteTitle.text = it.title
+                binding.tvLastUpdated.text =
+                    getString(R.string.last_updated, it.lastUpdated.toString())
+                binding.tvNoteText.text = it.text
+            }
+        })
+    }
+
+}
